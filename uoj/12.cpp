d45efe99ffc00af0af657abd907021b0dcd03895
Dev-XYS <xiaoyushuo0419@live.com>
2017-04-12 20:30:12 +0800

    简单问题：[UER#1] 猜数

---
+++ uoj/12.cpp
@@ -0,0 +1,27 @@
+// Problem Name: 猜数
+// Source: [UER#1] UOJ Round #1, Problem A
+
+#include <cstdio>
+#include <cmath>
+
+typedef long long ll;
+
+using namespace std;
+
+inline ll max(ll x, ll y)
+{
+	return x > y ? x : y;
+}
+
+int main()
+{
+	int T;
+	ll g, l;
+	scanf("%d", &T);
+	while (T--)
+	{
+		scanf("%lld%lld", &g, &l);
+		printf("%lld %lld\n", ((ll)sqrt(l / g) * g) << 1, (l / g + 1) * g);
+	}
+	return 0;
+}
