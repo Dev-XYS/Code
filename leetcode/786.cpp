f47c97501d1156a491534c31fc33648e92ceca4a
Dev-XYS <xiaoyushuo0419@live.com>
2018-03-25 14:07:31 +0800

    暴力，STL应用：K-th Smallest Prime Fraction

---
+++ leetcode/786.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+	struct sort_struct {
+		double f;
+		int p, q;
+		inline bool operator < (const sort_struct &x) {
+			return f < x.f;
+		}
+	};
+	
+	vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
+		const int n = A.size();
+		vector<sort_struct> F;
+		F.reserve(n * (n - 1) / 2);
+		int p = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				F.push_back((sort_struct){ (double)A[i] / A[j], A[i], A[j] });
+			}
+		}
+		nth_element(F.begin(), F.begin() + K - 1, F.end());
+		return { F[K - 1].p, F[K - 1].q };
+	}
+};
+
+int main()
+{
+	Solution S;
+	vector<int> A = { 1, 2, 3, 5 };
+	vector<int> ans = S.kthSmallestPrimeFraction(A, 3);
+	printf("%d %d\n", ans[0], ans[1]);
+	return 0;
+}
