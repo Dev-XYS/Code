ca3ffe8ffbe42116ea8473d9b7d1553e2f19fb18
Dev-XYS <xiaoyushuo0419@live.com>
2018-04-14 22:50:47 +0800

    判断链表中是否存在环（朴素做法）

---
+++ leetcode/141_Naive.cpp
@@ -0,0 +1,26 @@
+// Problem Name: Linked List Cycle
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *         int val;
+ *         ListNode *next;
+ *         ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+	bool hasCycle(ListNode *head) {
+		set<ListNode*> S;
+		for (ListNode *u = head; u != NULL; u = u->next)
+		{
+			if (S.count(u) > 0)
+			{
+				return true;
+			}
+			S.insert(u);
+		}
+		return false;
+	}
+}
