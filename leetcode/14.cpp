913ef34dc822cea160fdf4f49a0d5ccb5142b072
Dev-XYS <xiaoyushuo0419@live.com>
2019-03-18 16:57:23 +0800

    简单字符串处理问题：Longest Common Prefix

---
+++ leetcode/14.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    string longestCommonPrefix(string &x, string &y) {
+        string r;
+        for (int i = 0; i < min(x.size(), y.size()); i++)
+        {
+            if (x[i] == y[i]) r += x[i];
+            else break;
+        }
+        return r;
+    }
+
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 0) return "";
+        string r = strs[0];
+        for (int i = 1; i < (int)strs.size(); i++)
+        {
+            r = longestCommonPrefix(r, strs[i]);
+        }
+        return r;
+    }
+};
